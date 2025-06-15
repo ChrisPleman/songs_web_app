2854f56422a3874e16559b25d001453d79c0b8f8
Chris Pleman <chrispleman4@gmail.com>
2025-06-14 18:12:10 -0700

    I've added the beginnings of the route methods for the planned pages.

--- routes.py
+++ routes.py
@@ -6,3 +6,15 @@ from flask import render_template
 @app.route('/index')
 def home():
     return render_template('index.html', title='home')
+
+@app.route('/discover')
+def discover():
+    return render_template('discover.html', title='Discover')
+
+@app.route('/library')
+def library():
+    return render_template('library.html', title='My Library')
+
+@app.route('/account')
+def account():
+    return render_template('account.html', title='My Account')
